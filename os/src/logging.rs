a4f04ebffda27cd1fa924829137d437116409812
liujingx <liujingx21@mails.tsinghua.edu.cn>
2024-02-01 13:40:31 +0000

    feat: multi color log

--- os/src/logging.rs
+++ os/src/logging.rs
@@ -42,6 +42,7 @@ pub fn init() {
         Some("INFO") => LevelFilter::Info,
         Some("DEBUG") => LevelFilter::Debug,
         Some("TRACE") => LevelFilter::Trace,
-        _ => LevelFilter::Off,
+        Some("OFF") => LevelFilter::Off,
+        _ => LevelFilter::Info,
     });
 }
