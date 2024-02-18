0888c47e6497c953edfccaada6b792cbbf1ea259
liujingx <liujingx21@mails.tsinghua.edu.cn>
2024-02-18 04:51:47 +0000

    feat: sys write addr check

--- os/src/syscall/fs.rs
+++ os/src/syscall/fs.rs
@@ -1,15 +1,26 @@
+use crate::batch::{is_in_code_space, USER_STACK};
+
 const FD_STDOUT: usize = 1;
 
 pub fn sys_write(fd: usize, buf: *const u8, len: usize) -> isize {
     match fd {
         FD_STDOUT => {
+            let start = buf as usize;
+            let end = start + len - 1;
+            let in_code_space = is_in_code_space(start) && is_in_code_space(end);
+            let in_stack = USER_STACK.is_in_stack(start) && USER_STACK.is_in_stack(end);
+            if !in_stack && !in_code_space {
+                // [buf; len] exceeds current app space
+                return -1;
+            }
             let slice = unsafe { core::slice::from_raw_parts(buf, len) };
             let str = core::str::from_utf8(slice).unwrap();
             print!("{}", str);
             len as isize
-        },
+        }
         _ => {
-            panic!("Unsupported fd in sys_write!");
+            // Unsupported fd in sys_write
+            -1
         }
     }
 }
